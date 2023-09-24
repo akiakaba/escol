6e94e8bae1a0265203f33867ab572d965d6f77c0
Akihiro Akaba <11155356+akiakaba@users.noreply.github.com>
2023-09-24 18:35:20 +0900

    Add subject to query

--- gmu/q/builder.go
+++ gmu/q/builder.go
@@ -6,9 +6,10 @@ import (
 )
 
 type Builder struct {
-	After  string
-	Before string
-	From   string
+	After   string
+	Before  string
+	From    string
+	Subject string
 }
 
 var _ fmt.Stringer = Builder{}
@@ -23,6 +24,9 @@ func (b Builder) String() string {
 	}
 	if b.From != "" {
 		ss = append(ss, fmt.Sprintf("from:(%s)", b.From))
+	}
+	if b.Subject != "" {
+		ss = append(ss, fmt.Sprintf("subject:(%s)", b.Subject))
 	}
 	return strings.Join(ss, " ")
 }
